16c320054d3f64a5b6ddd52697550d129a251a82
Terry <terrybmochire@gmail.com>
2022-12-19 20:25:04 +0300

    Task 1.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = false;
+    // eslint-disable-next-line no-unused-vars
     const task2 = true;
   }
 
