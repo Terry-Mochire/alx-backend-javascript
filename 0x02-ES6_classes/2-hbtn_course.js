c0a447fe73fa5da7f931d5e272b93b894b742670
Terry <terrybmochire@gmail.com>
2022-12-22 23:19:45 +0300

    Task 2.

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,31 @@
+export default class HolbertonCourse {
+  constructor(name = String, length = Number, students = []) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
