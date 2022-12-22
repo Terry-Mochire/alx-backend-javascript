1ecf61626766aaf887bd6f5358fe5d1906670962
Terry <terrybmochire@gmail.com>
2022-12-22 17:50:17 +0300

    Advanvced task.

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -6,8 +6,8 @@ export default async function asyncUploadUser() {
   const obj = { photo, user };
 
   try {
-    obj.photo = `{status: ${photo.status}, body: ${photo.body}}`;
-    obj.user = `{firstName: ${user.firstName}, lastName: ${user.lastName}}`;
+    obj.photo = { status: photo.status, body: photo.body };
+    obj.user = { firstName: user.firstName, lastName: user.lastName };
   } catch (error) {
     obj.photo = null;
     obj.user = null;
