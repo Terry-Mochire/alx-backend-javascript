fbf19b4e79317685c43c37aa7b2e5f41189535b6
Terry <terrybmochire@gmail.com>
2022-12-23 00:08:15 +0300

    Task 2.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,20 +1,12 @@
 export default class HolbertonCourse {
-  constructor(name = String, length = Number, students = []) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
   }
 
   set name(name) {
@@ -22,13 +14,25 @@ export default class HolbertonCourse {
     this._name = name;
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(length) {
     if (typeof length !== 'number') throw TypeError('Length must be a number');
     this._length = length;
   }
 
+  get students() {
+    return this._students;
+  }
+
   set students(students) {
-    if (!Array.isArray(students) || students.every((student) => student !== 'string')) throw TypeError('Students must be an array of strings');
+    if (
+      !Array.isArray(students)
+        || students.every((student) => typeof student !== 'string')
+    ) throw TypeError('Students must be an array of strings');
+
     this._students = students;
   }
 }
